7eaf77fe6d51450077b36f3c17bbd02c7fee97db
mbranches <marcusvbranches@gmail.com>
2025-11-02 13:09:53 -0300

    commit inicial

---
+++ src/main/java/com/branches/MeudiariodeobrasApplication.java
@@ -0,0 +1,13 @@
+package com.branches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeudiariodeobrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeudiariodeobrasApplication.class, args);
+	}
+
+}
