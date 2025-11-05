43ba16e690d2e0946cd2b5aae33d4ff9b3878442
mbranches <marcusvbranches@gmail.com>
2025-11-05 15:02:26 -0300

    feat: adiciona GetObraDetailsByIdExternoController

---
+++ src/main/java/com/branches/obra/controller/GetObraDetailsByIdExternoController.java
@@ -0,0 +1,33 @@
+package com.branches.obra.controller;
+
+import com.branches.obra.dto.response.GetObraDetailsByIdExternoResponse;
+import com.branches.obra.service.GetObraDetailsByIdExternoService;
+import com.branches.security.model.UserDetailsImpl;
+import com.branches.shared.dto.UserDto.UserTenantDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class GetObraDetailsByIdExternoController {
+    private final GetObraDetailsByIdExternoService getObraDetailsByIdExternoService;
+
+    @GetMapping("/api/tenants/{tenantExternalId}/obras/{obraExternalId}")
+    public ResponseEntity<GetObraDetailsByIdExternoResponse> execute(@PathVariable String tenantExternalId,
+                                                                     @PathVariable String obraExternalId,
+                                                                     @AuthenticationPrincipal UserDetailsImpl user) {
+        List<UserTenantDto> userTenants = user.getUser().tenants();
+        List<Long> userAllowedObraIds = user.getUser().obrasPermitidasIds();
+
+        GetObraDetailsByIdExternoResponse response = getObraDetailsByIdExternoService.execute(obraExternalId, tenantExternalId, userTenants, userAllowedObraIds);
+
+        return ResponseEntity.ok(response);
+
+    }
+}
