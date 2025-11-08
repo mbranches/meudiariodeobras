6eea29ccb831fc9c6a6ccd36b1aca1918fc7a267
mbranches <marcusvbranches@gmail.com>
2025-11-08 10:55:12 -0300

    feat: implementa delete grupo de obra

---
+++ src/main/java/com/branches/obra/service/DeleteGrupoDeObraService.java
@@ -0,0 +1,41 @@
+package com.branches.obra.service;
+
+import com.branches.exception.ForbiddenException;
+import com.branches.obra.domain.GrupoDeObraEntity;
+import com.branches.obra.repository.GrupoDeObraRepository;
+import com.branches.tenant.service.GetTenantIdByIdExternoService;
+import com.branches.usertenant.domain.UserTenantEntity;
+import com.branches.usertenant.service.GetCurrentUserTenantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteGrupoDeObraService {
+    private final GetTenantIdByIdExternoService getTenantIdByIdExternoService;
+    private final GetCurrentUserTenantService getCurrentUserTenantService;
+    private final GetGrupoDeObraByIdAndTenantIdService getGrupoDeObraByIdAndTenantIdService;
+    private final GrupoDeObraRepository grupoDeObraRepository;
+
+    public void execute(Long id, String tenantExternalId, List<UserTenantEntity> userTenants) {
+        Long tenantId = getTenantIdByIdExternoService.execute(tenantExternalId);
+
+        UserTenantEntity currentUserTenant = getCurrentUserTenantService.execute(userTenants, tenantId);
+
+        checkIfUserHasAccessToGrupoDeObra(currentUserTenant);
+
+        GrupoDeObraEntity grupoDeObraEntity = getGrupoDeObraByIdAndTenantIdService.execute(id, tenantId);
+
+        grupoDeObraEntity.setAtivo(false);
+
+        grupoDeObraRepository.save(grupoDeObraEntity);
+    }
+
+    private void checkIfUserHasAccessToGrupoDeObra(UserTenantEntity currentUserTenant) {
+        if (!currentUserTenant.getAuthorities().getCadastros().getGrupoDeObras()) {
+            throw new ForbiddenException();
+        }
+    }
+}
