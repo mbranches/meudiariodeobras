6289d49a5607770ffd8df4b3e57e2f0ed32000f1
mbranches <marcusvbranches@gmail.com>
2025-11-23 12:01:07 -0300

    refactor: atualiza update relatorio

--- src/main/java/com/branches/relatorio/service/UpdateRelatorioService.java
+++ src/main/java/com/branches/relatorio/service/UpdateRelatorioService.java
@@ -5,10 +5,10 @@ import com.branches.exception.NotFoundException;
 import com.branches.obra.domain.ConfiguracaoRelatoriosEntity;
 import com.branches.obra.domain.ObraEntity;
 import com.branches.obra.repository.ObraRepository;
-import com.branches.relatorio.rdo.domain.*;
-import com.branches.relatorio.rdo.domain.enums.StatusRelatorio;
-import com.branches.relatorio.rdo.dto.request.*;
-import com.branches.relatorio.rdo.repository.*;
+import com.branches.relatorio.domain.RelatorioEntity;
+import com.branches.relatorio.dto.request.UpdateRelatorioRequest;
+import com.branches.relatorio.domain.enums.StatusRelatorio;
+import com.branches.relatorio.repository.RelatorioRepository;
 import com.branches.tenant.service.GetTenantIdByIdExternoService;
 import com.branches.usertenant.domain.UserTenantEntity;
 import com.branches.usertenant.service.GetCurrentUserTenantService;
@@ -25,15 +25,8 @@ public class UpdateRelatorioService {
     private final GetTenantIdByIdExternoService getTenantIdByIdExternoService;
     private final GetCurrentUserTenantService getCurrentUserTenantService;
     private final GetRelatorioByIdExternoAndTenantIdService getRelatorioByIdExternoAndTenantIdService;
-    private final CaracteristicaDePeriodoDoDiaRepository caracteristicaDePeriodoDoDiaRepository;
     private final RelatorioRepository relatorioRepository;
-    private final UpdateMaoDeObraDeRelatorioService updateMaoDeObraDeRelatorioService;
-    private final UpdateEquipamentosDeRelatorioService updateEquipamentosDeRelatorioService;
-    private final UpdateAtividadesDeRelatorioService updateAtividadesDeRelatorioService;
-    private final UpdateOcorrenciasDeRelatorioService updateOcorrenciasDeRelatorioService;
-    private final UpdateComentariosDeRelatorioService updateComentariosDeRelatorioService;
     private final ObraRepository obraRepository;
-    private final UpdateMateriaisDeRelatorioService updateMateriaisDeRelatorioService;
     private final CalculateHorasTotais calculateHorasTotais;
 
     @Transactional
@@ -59,47 +52,14 @@ public class UpdateRelatorioService {
         relatorio.setPrazoPraVencerObra(request.prazoPraVencer());
         updateStatus(currentUserTenant, relatorio, request.status());
 
-        if (configuracaoRelatorios.getShowCondicaoClimatica()) {
-            relatorio.setIndiciePluviometrico(request.indicePluviometrico());
-            relatorio.setCaracteristicasManha(getUpdatedCaracteristicaEntity(request.caracteristicasManha(), tenantId));
-            relatorio.setCaracteristicasTarde(getUpdatedCaracteristicaEntity(request.caracteristicasTarde(), tenantId));
-            relatorio.setCaracteristicasNoite(getUpdatedCaracteristicaEntity(request.caracteristicasNoite(), tenantId));
-        }
-
-        relatorioRepository.save(relatorio);
-
         if (configuracaoRelatorios.getShowHorarioDeTrabalho()) {
             relatorio.setHoraInicioTrabalhos(request.horaInicioTrabalhos());
             relatorio.setHoraFimTrabalhos(request.horaFimTrabalhos());
             relatorio.setHorasIntervalo(request.horasIntervalo());
             relatorio.setHorasTrabalhadas(calculateHorasTotais.execute(request.horaInicioTrabalhos(), request.horaFimTrabalhos(), request.horasIntervalo()));
-
-            relatorioRepository.save(relatorio);
         }
 
-        if (configuracaoRelatorios.getShowMaoDeObra()) {
-            updateMaoDeObraDeRelatorioService.execute(request.maoDeObra(), relatorio, tenantId);
-        }
-
-        if (configuracaoRelatorios.getShowEquipamentos()) {
-            updateEquipamentosDeRelatorioService.execute(request.equipamentos(), relatorio, tenantId);
-        }
-
-        if (configuracaoRelatorios.getShowAtividades()) {
-            updateAtividadesDeRelatorioService.execute(request.atividades(), relatorio, tenantId);
-        }
-
-        if (configuracaoRelatorios.getShowOcorrencias()) {
-            updateOcorrenciasDeRelatorioService.execute(request.ocorrencias(), relatorio, tenantId);
-        }
-
-        if (configuracaoRelatorios.getShowComentarios()) {
-            updateComentariosDeRelatorioService.execute(request.comentarios(), relatorio, tenantId);
-        }
-
-        if (configuracaoRelatorios.getShowMateriais()) {
-            updateMateriaisDeRelatorioService.execute(request.materiais(), relatorio, tenantId);
-        }
+        relatorioRepository.save(relatorio);
 
         //todo: gerar html do relatorio
         //todo: substituir relatorio pdf antigo
@@ -110,19 +70,6 @@ public class UpdateRelatorioService {
         if (status == StatusRelatorio.APROVADO && !currentUserTenant.getAuthorities().getRelatorios().getCanAprovar()) return;
 
         relatorio.setStatus(status);
-    }
-
-    private CaracteristicaDePeriodoDoDiaEntity getUpdatedCaracteristicaEntity(CaracteristicaDePeriodoDoDiaRequest request, Long tenantId) {
-        var id = request.id();
-
-        var entity = caracteristicaDePeriodoDoDiaRepository.findByIdAndTenantId(id, tenantId)
-                .orElseThrow(() -> new NotFoundException("Caracteristica de periodo do dia não encontrada com o id: " + id));
-
-        entity.setClima(request.clima());
-        entity.setCondicaoDoTempo(request.condicaoDoTempo());
-        entity.setAtivo(request.ativo());
-
-        return entity;
     }
 
     private void checkIfUserCanUpdateRelatorio(UserTenantEntity currentUserTenant) {
