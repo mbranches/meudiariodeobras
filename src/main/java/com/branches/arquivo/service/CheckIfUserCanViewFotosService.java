0208b4b1bf5d50dbc3644cbcedb7b52dc85a066c
mbranches <marcusvbranches@gmail.com>
2025-11-25 19:10:15 -0300

    fix: update permission check to validate photo access for user

--- src/main/java/com/branches/arquivo/service/CheckIfUserCanViewFotosService.java
+++ src/main/java/com/branches/arquivo/service/CheckIfUserCanViewFotosService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CheckIfUserCanViewFotosService {
     public void execute(UserTenantEntity userTenant) {
-        if (userTenant.getAuthorities().getItensDeRelatorio().getAtividades()) return;
+        if (userTenant.getAuthorities().getItensDeRelatorio().getFotos()) return;
 
         throw new ForbiddenException();
     }
