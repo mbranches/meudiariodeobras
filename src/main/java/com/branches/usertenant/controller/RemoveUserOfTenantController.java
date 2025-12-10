ec70648136ba57aa9f2e75954391a035935f1e21
mbranches <marcusvbranches@gmail.com>
2025-12-10 19:01:37 -0300

    feat: atualiza remove user of tenant pra retornar 204

--- src/main/java/com/branches/usertenant/controller/RemoveUserOfTenantController.java
+++ src/main/java/com/branches/usertenant/controller/RemoveUserOfTenantController.java
@@ -4,6 +4,7 @@ import com.branches.config.security.UserTenantsContext;
 import com.branches.usertenant.domain.UserTenantEntity;
 import com.branches.usertenant.service.RemoveUserOfTenantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,11 @@ public class RemoveUserOfTenantController {
     private final RemoveUserOfTenantService removeUserOfTenantService;
 
     @DeleteMapping("/api/tenants/{tenantExternalId}/users/{userExternalId}")
-    public void execute(@PathVariable String tenantExternalId, @PathVariable String userExternalId) {
+    public ResponseEntity<Void> execute(@PathVariable String tenantExternalId, @PathVariable String userExternalId) {
         List<UserTenantEntity> userTenants = UserTenantsContext.getUserTenants();
 
         removeUserOfTenantService.execute(tenantExternalId, userExternalId, userTenants);
+
+        return ResponseEntity.noContent().build();
     }
 }
